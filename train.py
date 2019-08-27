c6d65aa7494b8e687244087241c16a517c786119
yysys <542953285@qq.com>
2019-08-27 10:37:28 +0800

    code

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     model.compile("adagrad", "binary_crossentropy", loss_weights=loss_weights, )
 
     history = model.fit(train_model_input, train_labels,
-                        batch_size=4096, epochs=1, verbose=1)
+                        batch_size=4096, epochs=5, verbose=1)
     pred_ans = model.predict(test_model_input, batch_size=2 ** 14)
 
     # test_auc = metrics.roc_auc_score(test[], prodict_prob_y)
