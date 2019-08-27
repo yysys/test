7322bdaed915659d712f0e31e9d0d1b543cba933
yysys <542953285@qq.com>
2019-08-27 21:36:26 +0800

    code

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from deepctr.inputs import  SparseFeat, DenseFeat,get_fixlen_feature_names
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]='6'
 
-loss_weights = [0.01, 0.02, ]  # [0.7,0.3]任务权重可以调下试试
+loss_weights = [1, 1, ]  # [0.7,0.3]任务权重可以调下试试
 VALIDATION_FRAC = 0.2  # 用做线下验证数据比例
 
 def change_time(timeStamp):
@@ -83,7 +83,7 @@ if __name__ == "__main__":
                   loss_weights=loss_weights)
 
     history = model.fit(train_model_input, train_labels,
-                        batch_size=4096, epochs=20, verbose=1)
+                        batch_size=256, epochs=20, verbose=1)
     pred_ans = model.predict(test_model_input, batch_size=2 ** 10)
 
     # test_auc = metrics.roc_auc_score(test[], prodict_prob_y)
