cfc3cb4ee95ae52eba0c667376b039c163553cdc
yysys <542953285@qq.com>
2019-08-28 21:41:45 +0800

    code

--- like_task.py
+++ like_task.py
@@ -3,8 +3,8 @@ import time, datetime
 #from deepctr import SingleFeat
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from sklearn import metrics
-#from like_model import xDeepFM_MTL
-from deepctr.models import xDeepFM
+from like_model import xDeepFM_MTL
+#from deepctr.models import xDeepFM
 import keras
 from deepctr.inputs import  SparseFeat, DenseFeat,get_fixlen_feature_names
 
@@ -78,7 +78,7 @@ if __name__ == "__main__":
 
     test_model_input = [test[name] for name in feature_names]
 
-    model = xDeepFM(linear_feature_columns, dnn_feature_columns)
+    model = xDeepFM_MTL(linear_feature_columns, dnn_feature_columns)
     model.compile(optimizer="adagrad", loss=keras.losses.binary_crossentropy, metrics=['binary_crossentropy'])
 
     history = model.fit(train_model_input, train_labels,
